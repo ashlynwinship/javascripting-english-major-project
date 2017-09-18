d1e0c480163f69af69dbd8b17b50f1ee8696da83
ashlynwinship <ashlynwinship@gmail.com>
2017-09-18 10:03:52 -0400

    create scripts.js pt. 2

--- scripts.js
+++ scripts.js
@@ -1,3 +1,1 @@
-let userInput;
-userInput = "samosa";
-if ( userInput === "burrito" ) { $("#response").html("Brilliant choice!"); } else { $("#response").html("Don't you want a burrito?"); }
+alert("scripts.js has loaded!");
