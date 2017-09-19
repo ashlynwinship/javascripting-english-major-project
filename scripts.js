34d9f2e3e81e5b45bcbc684af90c3f49d9b40e47
ashlynwinship <ashlynwinship@gmail.com>
2017-09-19 15:06:42 -0400

    fixed jQuery

--- scripts.js
+++ scripts.js
@@ -1,3 +1,1 @@
-let userInput;
-userInput = "samosa";
-if ( userInput === "burrito") { $("#response").html("Brilliant choice!"); } else { $("#response").html("Don't you want a burrito?"); } 
+$("#response").html("scripts.js has loaded!");
