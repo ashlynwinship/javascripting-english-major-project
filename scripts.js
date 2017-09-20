7f41aa316e43c7f533a18c02c892effec934fcc1
ashlynwinship <ashlynwinship@gmail.com>
2017-09-19 22:17:30 -0400

    HW 3

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-for ( let i = 1; i < 4; i = i + 1 ) { $("#response").append("<br />" + i); }
+let userInput;
+userInput = prompt("Choose a number.", "Type your answer here.");
+let i;
+i = 1;
+for ( let i = 1; i <= userInput; i = i + 2 ) { $("#response").append("<br />" + i + " is odd."); }
+for ( let i = 2; i <= userInput; i = i + 2 ) { $("#response").append("<br />" + i + " is even."); }
