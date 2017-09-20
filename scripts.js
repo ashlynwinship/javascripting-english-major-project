6c4b7e48ef9c584b24dc59657be09b468fe882ae
ashlynwinship <ashlynwinship@gmail.com>
2017-09-19 22:28:53 -0400

    hw 3 correctly complete

--- scripts.js
+++ scripts.js
@@ -2,5 +2,4 @@ let userInput;
 userInput = prompt("Choose a number.", "Type your answer here.");
 let i;
 i = 1;
-for ( let i = 1; i <= userInput; i = i + 2 ) { $("#response").append("<br />" + i + " is odd."); }
-for ( let i = 2; i <= userInput; i = i + 2 ) { $("#response").append("<br />" + i + " is even."); }
+for ( let i = 1; i <= userInput; i = i + 1 ) { if ( i % 2 === 0 ) { $("#response").append("<br />" + i + " is even."); } if ( i % 2 === 1 ) { $("#response").append("<br />" + i + " is odd."); } }
