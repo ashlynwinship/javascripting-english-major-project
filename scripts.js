4301be7c85d686fdb4e26b482735ad148ef7d18a
ashlynwinship <ashlynwinship@gmail.com>
2017-09-25 15:49:56 -0400

    hw 4

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
-let tipCalculator;
-tipCalculator = function(total, tipRate){ let tipAmount; tipAmount = tipRate * total; $("#response").html("Your tip is $" + tipAmount); }
-tipCalculator(50.00, 0.2);
+let tipCalculator
+tipCalculator = function(total, tipRate) { let tipAmount; tipAmount = tipRate * total; let tipTotal; tipTotal = tipRate * 0.01 * total; if (tipRate < 1) { $("#response").html("Your tip is $" + tipAmount); } else { $("#response").html("Your tip is $" + tipTotal); } };
+tipCalculator(50.00, 20);
