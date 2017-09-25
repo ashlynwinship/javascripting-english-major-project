f878ffa6aabf67a1bd35d9dc00fb3f5616b4993e
ashlynwinship <ashlynwinship@gmail.com>
2017-09-25 10:30:49 -0400

    tipCalculator

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-let userInput;
-userInput = prompt("Choose a number.", "Type your answer here.");
-let i;
-i = 1;
-for ( let i = 1; i <= userInput; i = i + 1 ) { if ( i % 2 === 0 ) { $("#response").append("<br />" + i + " is even."); } if ( i % 2 === 1 ) { $("#response").append("<br />" + i + " is odd."); } }
+let tipCalculator;
+tipCalculator = function(total, tipRate){ let tipAmount; tipAmount = tipRate * total; $("#response").html("Your tip is $" + tipAmount); }
+tipCalculator(50.00, 0.2);
