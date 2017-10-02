3a5b5ef93b9d8ce649d87c13bf045c902be96f2f
ashlynwinship <ashlynwinship@gmail.com>
2017-10-02 11:54:44 -0400

    hw 5 almost there

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-let myHabaneroSauceSquirts, myBurritoObject;
-myHabaneroSauceSquirts = 3;
-myBurritoObject = { tortilla: "wheat", guacamole: true, beans: "pinto", habaneroSauceSquirts: myHabaneroSauceSquirts, spiciness: function(){ if (myHabaneroSauceSquirts > 0){alert("This is a spicy burrito!"); } else {alert("This is a mild burrito.");} } };
-$("#response").html("Your burrito has " + myBurritoObject.habaneroSauceSquirts + " squirts of habanero.");
-myBurritoObject.spiciness();
+let userInput, myObject;
+userInput = prompt("Enter your array here.");
+let lastItem;
+lastItem = userInput[userInput.length-1];
+$("#response").html("The last item in your array is " + lastItem + ".");
