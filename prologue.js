f92c1205f89a80ec17f98a9c9be3ef9c66882549
ashlynwinship <ashlynwinship@gmail.com>
2017-10-31 10:11:30 -0400

    JS Ch 9 pt. 3

--- prologue.js
+++ prologue.js
@@ -3,9 +3,17 @@ $("#glosses").html("<p>The glosses will go here.</p>");
 
 $("#glosses").html("<p>The glosses will go here.</p>");
 let line1, line1Text;
-line1 = [{text: "Whan"}, {text: "that"}, {text: "Aprill,"}, {text: "with"},
-        {text: "his"}, {text: "shoures"}, {text: "soote"}];
-line1Text = line1.map(function(word){
-  return word.text;
-}).join(" ");
-$("#prologue").html("<p>" + line1Text + "<br /></p>");
+line1 = [{text: "Whan", modern: "When"}, {text: "that"}, {text: "Aprill,",
+        modern: "April,"}, {text: "with"}, {text: "his"}, {text: "shoures",
+        modern: "showers"}, {text: "soote", modern: "sweet"}];
+line1Text = "<blockquote><p>";
+line1.forEach(function(word){
+  let wordString;
+  wordString = word.text;
+  if (word.modern){
+    wordString = "<a href='#'>" + wordString + "</a>";
+  }
+  line1Text = line1Text + wordString + " ";
+});
+line1Text = line1Text + "<br />(line 2 would go here)</p></blockquote>";
+$("#prologue").html(line1Text);
