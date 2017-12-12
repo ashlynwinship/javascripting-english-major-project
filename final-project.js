330ed368421e2a197c44d3c915fe34f2e61df753
ashlynwinship <ashlynwinship@gmail.com>
2017-12-12 13:33:42 -0500

    rearrange page

--- final-project.js
+++ final-project.js
@@ -9,15 +9,7 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 tileLayer.addTo(deriveMap);
 deriveMap.setView([40.722622, -74.000849], 14);
 
-let featuresLayer;
-featuresLayer = L.featureGroup(featuresLayer.map(function)(feature){
-  let popupContent;
-  popupContent = "<h4>" + feature.name + "</h4>";
-  popupContent = popupContent + "Read about" + feature.name + " on <a href='" + feature.wikipedia + "'>Wikipedia</a>.";
-  return L.marker(feature.latLng).bindPopup(popupContent);
-})
-);
-
+//change colors on markers for starting points of derives
 let stLukesMarker, pier40, pier45, hollandTunnel, aveOfAmericas;
 stLukesMarker = L.marker([40.730391, -74.006171]).addTo(deriveMap).bindPopup("Starting point for Dérive 1");
 pier40 = L.marker([40.729419, -74.011886]).addTo(deriveMap).bindPopup("Pier 40");
@@ -36,7 +28,7 @@ derive1Polyline = L.polyline(derive1, {color: "#cccc00"}
 
 let pilates, pilatesMarker, miscDerive1;
 pilates = [40.730048, -73.998508];
-pilatesMarker = L.marker(pilates).addTo(deriveMap).bindPopup("Something here reminded me of pilates.");
+pilatesMarker = L.marker(pilates).addTo(deriveMap).bindPopup("Pilates place");
 miscDerive1 = [[40.722012, -74.005449],
                [40.730048, -73.998508]];
 let miscDerive1Line;
@@ -44,22 +36,22 @@ miscDerive1Line = L.polyline(miscDerive1, {color: "#ff0000"}
                 ).addTo(deriveMap);
 
 let worthStMarker, wBroadwayCorner, worldTradeCenter, oculus, stPaulsChapel, cityHallPark;
-worthStMarker = L.circleMarker([40.717097, -74.006755]);
+worthStMarker = L.marker([40.717097, -74.006755]);
 worthStMarker.addTo(deriveMap);
 worthStMarker.bindPopup("Starting point for Dérive 2");
-wBroadwayCorner = L.circleMarker([40.717609, -74.007571]);
+wBroadwayCorner = L.marker([40.717609, -74.007571]);
 wBroadwayCorner.addTo(deriveMap);
 wBroadwayCorner.bindPopup("'Head toward the heart of the city.'");
-worldTradeCenter = L.circleMarker([40.711801, -74.01312]);
+worldTradeCenter = L.marker([40.711801, -74.01312]);
 worldTradeCenter.addTo(deriveMap);
 worldTradeCenter.bindPopup("One World Trade Center");
-oculus = L.circleMarker([40.711591, -74.011482]);
+oculus = L.marker([40.711591, -74.011482]);
 oculus.addTo(deriveMap);
 oculus.bindPopup("The Oculus");
-stPaulsChapel = L.circleMarker([40.71132, -74.009185]);
+stPaulsChapel = L.marker([40.71132, -74.009185]);
 stPaulsChapel.addTo(deriveMap);
 stPaulsChapel.bindPopup("St. Paul's Cathedral");
-cityHallPark = L.circleMarker([40.71285, -74.006465]);
+cityHallPark = L.marker([40.71285, -74.006465]);
 cityHallPark.addTo(deriveMap);
 cityHallPark.bindPopup("City Hall Park");
 let derive2;
@@ -74,8 +66,8 @@ derive2Polyline = L.polyline(derive2, {color: "#cc00ff"}
                     ).addTo(deriveMap);
 
 let subStation, lafAndWorth, miscDerive2;
-subStation = L.circleMarker([40.713011, -74.00328]).addTo(deriveMap).bindPopup("Brooklyn Bridge-City Hall/Chambers Street Station");
-lafAndWorth = L.circleMarker([40.715496, -74.003032]).addTo(deriveMap).bindPopup("Turn here onto Worth.");
+subStation = L.marker([40.713011, -74.00328]).addTo(deriveMap).bindPopup("Brooklyn Bridge-City Hall/Chambers Street Station");
+lafAndWorth = L.marker([40.715496, -74.003032]).addTo(deriveMap).bindPopup("Turn here onto Worth.");
 miscDerive2 = [[40.713011, -74.00328],
                [40.715496, -74.003032],
                [40.717097, -74.006755]];
@@ -83,35 +75,9 @@ let miscDerive2Line;
 miscDerive2Line = L.polyline(miscDerive2, {color: "#ff0000"}
                     ).addTo(deriveMap);
 
-
-let juliusNYC, juliusTileLayer;
-juliusNYC = L.map("second-map");
-juliusTileLayer =
-L.juliusTileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
-        attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
-        subdomains: "abcd",
-        maxZoom: 18
-    });
-juliusTileLayer.addTo(juliusNYC);
-juliusNYC.setView([40.722622, -74.000849], 14);
-
-//define markers for Julius's NYC.
-let canalStMarker;
-canalStMarker = L.marker([40.722012, -74.005449]).addTo(deriveMap).bindPopup("Canal Street");
+//define Julius's markers
 
 
-let juliusWorld, worldTileLayer;
-juliusWorld = L.map("third-map");
-worldTileLayer =
-L.worldTileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
-      attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
-      subdomains: "abcd",
-      maxZoom: 18
-    });
-worldTileLayer.addTo(juliusWorld);
-juliusWorld.setView([40.722622, -74.000849], 14);
-
-//define markers for Julius's world.
 
 let md;
 md = window.markdownit({html: true}).use(window.markdownitFootnote);
